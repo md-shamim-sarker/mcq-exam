a6fb86207ddfb6f0ec972993f7d1f9e23d235e5c
shamimspro <shamimspro@gmail.com>
2022-10-11 00:18:27 +0600

    made navbar responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,14 +6,18 @@ import {ChartPieIcon, HomeIcon, QuestionMarkCircleIcon, UserGroupIcon} from '@he
 const Navbar = () => {
     return (
         <div className='bg-pink-500 text-pink-50 py-2'>
-            <div className='w-4/5 mx-auto flex justify-between items-center'>
+            <div className='w-4/5 mx-auto flex justify-center lg:justify-between items-center'>
+
+                {/* Site Logo for both desktop and mobile */}
                 <div className='text-2xl font-extrabold'>
                     <Link className='flex gap-x-2 items-center' to={"/"}>
                         <img src={Logo} alt="logo_image" className='w-10 border border-pink-50 rounded-full' />
                         <h2>MCQ EXAM</h2>
                     </Link>
                 </div>
-                <div className='flex gap-x-5'>
+
+                {/* For Desktop Device */}
+                <div className='hidden lg:flex gap-x-5'>
                     <Link className='flex gap-x-1 hover:underline underline-offset-8' to={"/"}>
                         <HomeIcon className='w-5'></HomeIcon>
                         <span>Home</span>
@@ -28,6 +32,26 @@ const Navbar = () => {
                     </Link>
                     <Link className='flex gap-x-1 hover:underline underline-offset-8' to={"/about"}>
                         <UserGroupIcon className='w-5'></UserGroupIcon>
+                        <span>About</span>
+                    </Link>
+                </div>
+
+                {/* For Mobile Device */}
+                <div className='w-screen flex justify-around lg:hidden fixed bottom-0 bg-pink-500 py-1'>
+                    <Link className='flex flex-col justify-center items-center' to={"/"}>
+                        <HomeIcon className='w-7'></HomeIcon>
+                        <span>Home</span>
+                    </Link>
+                    <Link className='flex flex-col justify-center items-center' to={"/statistics"}>
+                        <ChartPieIcon className='w-7'></ChartPieIcon>
+                        <span>Statistics</span>
+                    </Link>
+                    <Link className='flex flex-col justify-center items-center' to={"/blog"}>
+                        <QuestionMarkCircleIcon className='w-7'></QuestionMarkCircleIcon>
+                        <span>Blog</span>
+                    </Link>
+                    <Link className='flex flex-col justify-center items-center' to={"/about"}>
+                        <UserGroupIcon className='w-7'></UserGroupIcon>
                         <span>About</span>
                     </Link>
                 </div>
